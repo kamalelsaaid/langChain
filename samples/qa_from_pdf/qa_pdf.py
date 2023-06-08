1ecb024a73ceabf1dfddbd23ca32187e531ee3b6
kamalelsaaid <kamal.zakieldin@arete-global.com>
2023-06-08 22:02:57 +0300

    sample: qa to answer from pdf

---
+++ samples/qa_from_pdf/qa_pdf.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+api_key  = os.getenv('OPENAI_API_KEY')
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+
+if __name__ == "__main__":
+    print("hi")
+    pdf_path = "./papers/react.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=30, separator="\n"
+    )
+    docs = text_splitter.split_documents(documents=documents)
+
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore.save_local("./localVS/faiss_index_react")
+
+    new_vectorstore = FAISS.load_local("./localVS/faiss_index_react", embeddings)
+    qa = RetrievalQA.from_chain_type(
+        llm=OpenAI(), chain_type="stuff", retriever=new_vectorstore.as_retriever()
+    )
+    res = qa.run("Give me the gist of ReAct in 3 sentences")
+    print(res)
